78afbbc8dc703499e2a2090a92ec20a7135aa912
Jon Craton <jncraton@gmail.com>
2025-03-26 16:58:47 -0400

    Wrap loader code

--- examples/torch-mnist/mnist.py
+++ examples/torch-mnist/mnist.py
@@ -2,15 +2,20 @@ import torch
 import torch.nn as nn
 from torchvision import datasets, transforms
 
-train_dataset = datasets.MNIST(
-    root="./data", train=True, download=True, transform=transforms.ToTensor()
-)
-test_dataset = datasets.MNIST(
-    root="./data", train=False, download=True, transform=transforms.ToTensor()
-)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=True)
+def get_loader(is_train):
+    dataset = datasets.MNIST(
+        root="./data",
+        train=is_train,
+        download=True,
+        transform=transforms.ToTensor(),
+    )
+
+    return torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+
+
+train_loader = get_loader(is_train=True)
+test_loader = get_loader(is_train=False)
 
 
 class Net(nn.Module):
